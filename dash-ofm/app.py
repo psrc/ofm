93af72172bac5ec441114ac121e137cdbebc3420
Christy Lam <cyyllam@gmail.com>
2019-11-07 17:17:08 -0800

    utilize dash bootstrap components

--- dash-ofm/app.py
+++ dash-ofm/app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly
 import plotly.graph_objects as go
@@ -10,6 +11,7 @@ import pyodbc
 import numpy as np
 
 cnty_code_dict = {'033':'King', '035':'Kitsap', '053':'Pierce', '061':'Snohomish'}
+
 def sqlconn(dbname):
     # create Elmer connection
     con = pyodbc.connect('DRIVER={SQL Server};SERVER=AWS-PROD-SQL\COHO;DATABASE=' + dbname + ';trusted_connection=true')
@@ -25,65 +27,84 @@ def query_tblOfmSaep():
     df['CountyName'] = df['CountyID'].map(cnty_code_dict)
     return(df)
 
+#def create_growth_tblOfmSaep(table):
+#    table = table.sort_values(by='Year')
+
 df = query_tblOfmSaep()
+#df.sort_values(by='Year')
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
 
-app.layout = html.Div(children=[
+banner = dbc.Jumbotron(
+    [
+        dbc.Container(
+            [
+                html.H1('Office of Financial Management (OFM), Small Area Estimates', className="display-head"),
+                html.H2('For the Central Puget Sound Region', className="lead")
+            ],
+            fluid=True
+        )
+    ],
+    fluid=True
+)
 
-    html.Header(className="jumbotron", children=[ 
-        html.H1(className="display-head",
-        children='Office of Financial Management (OFM), Small Area Estimates'
-        ),
+cnty_checklist = dbc.FormGroup(
+    [
+         html.Legend("County"),
+         dbc.Checklist(
+            id='county-id-checklist',
+            options=[
+                 {'label':'King', 'value':'033'},
+                 {'label':'Kitsap', 'value':'035'},
+                 {'label':'Pierce', 'value':'053'},
+                 {'label':'Snohomish', 'value':'061'}
+             ],
+            value=['033', '035', '053', '061'],
+            className = 'selector'
+        )
+    ] 
+)
 
-        html.H2(className="lead", children='''
-                Counties in the Central Puget Sound Region
-            ''')]),
+est_type_radioitem = dbc.FormGroup(
+    [
+         html.Legend("Estimate Type"),
+         dbc.RadioItems(
+            id='estimate-type-radioitem',
+            options=[
+                {'label':'Total Population', 'value':'Total Population'},
+                {'label':'Household Population', 'value':'Household Population'},
+                {'label':'Group Quarter Population', 'value':'Group Quarter Population'},
+                {'label':'Household', 'value':'Household'},
+                {'label':'Housing Unit', 'value':'Housing Unit'}
+                ],
+            value='Total Population',
+            className = 'selector'
+        )
+    ]
+)
 
-    
-    html.Div(className='content-box', children=[
-            html.Aside(className="card border-secondary mb-3", children=[
-                html.Section(className="form-check", children=[ 
-                    html.Legend('County'),
-                      dcc.Checklist(id='county-id-checklist',
-                                  className="selector",
-                        options=[
-                            {'label':'King', 'value':'033'},
-                            {'label':'Kitsap', 'value':'035'},
-                            {'label':'Pierce', 'value':'053'},
-                            {'label':'Snohomish', 'value':'061'}
-                            ],
-                        value=['033', '035', '053', '061']
-                      )]
-                      ), # end Section
-                html.Section(className="form-check", children=[ 
-                    html.Legend('Estimate Type'),
-                    dcc.RadioItems(id='estimate-type-radioitem',
-                                   className="selector",
-                        options=[
-                            {'label':'Total Population', 'value':'Total Population'},
-                            {'label':'Household Population', 'value':'Household Population'},
-                            {'label':'Group Quarter Population', 'value':'Group Quarter Population'},
-                            {'label':'Household', 'value':'Household'},
-                            {'label':'Housing Unit', 'value':'Housing Unit'}
-                            ],
-                        value='Total Population'
-              )])         
-            ]) # end Aside
-            , 
-        html.Main(children=[
-            dcc.Graph(id='county-graph')
-            ]) # end Main
-    ]) # end Div.content-box
-     
-]) # end parent
+body = dbc.Container(
+    [
+ 
+        dbc.Row(
+            [
+                 dbc.Col(className="card border-secondary mb-3", children=[dbc.Form([cnty_checklist, est_type_radioitem])], width={"size":2}),
+                 dbc.Col(dcc.Graph(id='county-graph'), width=10)
+            ]
+         )
+    ],
+    className="body",
+    fluid=True
+)
+
+app.layout = html.Div([banner, body])
+
 
 @app.callback(
        Output(component_id='county-graph', component_property='figure'),
        [Input(component_id='estimate-type-radioitem', component_property='value'),
         Input(component_id='county-id-checklist', component_property='value')
-        #Input(component_id='county-id-radioitem', component_property='value')
-        ]
+       ]
        )
 def update_county_graph(attribute, countyids):
     filtered_df = df[(df.AttributeDesc == attribute) & (df['CountyID'].isin(countyids))]
