d797a916319b46d079c601c59cb2578f2ae85f52
Christy Lam <cyyllam@gmail.com>
2021-03-05 16:56:05 -0800

    revised original script

--- youth-pop-cmp/ofm_forecast_youth_5-17.R
+++ youth-pop-cmp/ofm_forecast_youth_5-17.R
@@ -2,53 +2,54 @@
 # 15-17 age cohort based on the share of persons age 15-17 in X
 
 library(openxlsx)
-library(dplyr)
 library(data.table)
+library(here)
+library(stringr)
 
-# function to assemble OFM tables for each geography
-assemble.juris.table <- function(jurisdiction, start.row){
-  master.file <- file.path(indir2, ofm.file)
-  section1 <- read.xlsx(master.file, sheet = 1, startRow = as.integer(start.row), colNames = TRUE, rowNames = FALSE, rows = c(as.integer(start.row):as.integer(start.row + 20)), cols = NULL)
-  section2 <- read.xlsx(master.file, sheet = 1, startRow = as.integer(start.row + 22), colNames = TRUE, rowNames = FALSE, rows = c(as.integer(start.row + 22): as.integer(start.row + 42)), cols = NULL)
-  section3 <- read.xlsx(master.file, sheet = 1, startRow = as.integer(start.row + 44), colNames = TRUE, rowNames = FALSE, rows = c(as.integer(start.row + 44): as.integer(start.row + 64)), cols = c(1:2))
-  table <- bind_cols(section1, section2, section3)
-  colnames(table)[1] <- "agegroups"
-  select.cols <- (colnames(table)[grepl(paste0("groups|\\d+{4}"), names(table))])
-  table <- table[,colnames(table) %in% select.cols]
-  colnames(table)[2:ncol(table)] <- paste0("tot", select.cols[2:length(select.cols)])
-  table <- table[-c(1), ]
-  table$jurisdiction <- jurisdiction
-  table %>% mutate_each(funs(as.numeric), starts_with("tot"))
-} 
 
-# function to append worksheet
+# functions ---------------------------------------------------------------
+
+
 append.worksheet <- function(table, workbook, sheetName){
+  # function to append worksheet
   addWorksheet(workbook, sheetName)
   writeData(workbook, sheet = sheetName, x = table, colNames = TRUE)
-  saveWorkbook(workbook, file.path(indir, my.filename), overwrite = TRUE )
+  saveWorkbook(workbook, here('youth-pop-cmp', 'results', out.filename), overwrite = TRUE )
 }
 
-indir <- "J:/Staff/Christy/OFM/forecasts/requests/jean"
-indir2 <- "J:/Staff/Christy/OFM/forecasts"
-my.filename <- "pop_projections_rev_2017_09_07_3.xlsx"
 
-ofm.state.proj <- "stfc_2016.xlsx"
-ofm.file <- "gma2012_cntyage_med.xlsx"
+# input -------------------------------------------------------------------
 
-#read state forecast
-osp <- read.xlsx(file.path(indir2, ofm.state.proj), sheet = 'Single_Year', startRow = 9, colNames = TRUE)
-forecast.years <- c(2010, 2015, 2025, 2040)
-yr.cols <- paste0("Total.", forecast.years)
-yr.colnames <- paste0("tot", forecast.years)
-age <- c(seq(15,19))
 
-# filter state forecast
-osp.select <- osp %>% 
-  filter(Age %in% age) %>% 
-  select(Age, one_of(yr.cols)) %>% as.data.table()
+data.dir <- here('youth-pop-cmp', 'data')
+out.filename <- paste0("pop_projections_", Sys.Date(),".xlsx")
 
-setnames(osp.select, yr.cols, paste0("tot", forecast.years))
-osp.dt <- dcast.data.table(melt(osp.select, id.vars = 'Age'), variable ~ Age)
+ofm.state.proj <- "stfc_population_by_age_and_sex.xlsx"
+ofm.file <- "gma_2017_age_sex_med_2050.xlsx"
+
+gma.years <- c(2030, 2050)
+state.years <- c(2030, 2040)
+
+look.up <- data.table(gma = paste0("Total_", gma.years), state = paste0("Total_", state.years), id = 1:length(gma.years))
+
+
+# state forecast ----------------------------------------------------------
+
+
+## clean state forecast ----
+osp <- read.xlsx(file.path(data.dir, ofm.state.proj), sheet = 'Single Year', startRow = 9)
+setDT(osp)
+
+state.yr.cols <- paste0("Total.", state.years)
+state.cols <- c("Age", state.yr.cols)
+state.age <- c(seq(15,19))
+
+## filter state forecast ----
+
+osp <- osp[Age %in% state.age, ..state.cols]
+colnames(osp) <- str_replace_all(colnames(osp), "\\.", "_")
+
+osp.dt <- dcast(melt(osp, id.vars = 'Age'), variable ~ Age)
 setnames(osp.dt, colnames(osp.dt), c("year", paste0("pop_", 15:19)))
 osp.dt1 <- osp.dt[, pop_15_19 := rowSums(.SD), .SDcols = 2:6
                   ][, pop_15_17 := rowSums(.SD), .SDcols = 2:4
@@ -57,66 +58,100 @@ osp.dt1 <- osp.dt[, pop_15_19 := rowSums(.SD), .SDcols = 2:6
                       ][, share_pop18_19 := pop_18_19/pop_15_19
                         ]
 
-# assemble regional GMA county forecasts
-king <- assemble.juris.table("King County", 1176)
-kitsap <- assemble.juris.table("Kitsap County", 1245)
-pierce <- assemble.juris.table("Pierce County", 1866)
-snohomish <- assemble.juris.table("Snohomish County", 2142)
-region <- bind_rows(king, kitsap, pierce, snohomish)
+
+# GMA county forecast ----------------------------------------------------
+
+## clean county forecast ----
+
+gma <- read.xlsx(here(data.dir, ofm.file), startRow = 4, colNames = FALSE, fillMergedCells = TRUE)
+setDT(gma)
+keep.cols <- colnames(gma)[which(!is.na(gma[2, ]))]
+
+gma <- gma[, ..keep.cols]
+
+# create new column headers
+r1 <- as.character(gma[1, 3:ncol(gma)])
+r2 <- as.character(gma[2, 3:ncol(gma)])
+new.gma.cols <- c("County", "Age", paste(r2, r1, sep = "_"))
+
+setnames(gma, colnames(gma), new.gma.cols)
+
+## filter county forecast ----
+
+region <- gma[County %in% c("King", "Kitsap", "Pierce", "Snohomish")]
+region <- region[, lapply(.SD, as.numeric), .SDcols = str_subset(colnames(region), "\\d+$"), by = c("Age", "County")]
 
 # isolate only 15-19 cohort
-region.youth <- as.data.table(region)
-ry.m <- melt(region.youth, id.vars = c("agegroups", "jurisdiction"), measure.vars = c("tot2010", "tot2015", "tot2020",  "tot2025", "tot2030", "tot2035","tot2040"))
-ry.m.select <- ry.m[agegroups == "15-19" & variable %in% paste0("tot", forecast.years),]
+ry.m <- melt(region, id.vars = c("Age", "County"), measure.vars = str_subset(colnames(region), "^Total"))
+ry.m.select <- ry.m[Age == "15-19" & variable %in% paste("Total", gma.years, sep = "_"),
+                    ]
+
+## join ----
+
+# apply id to both gma and state youth tables
+ry.m.select <- ry.m.select[look.up[, .(gma, id)], on = c("variable" = "gma")]
+osp.dt1 <- osp.dt1[look.up[, .(state, id)], on = c("year" = "state")]
 
 # 'join' on years and apply shares creating two new fields in ry.m.select 
-setkey(ry.m.select, variable)[osp.dt1, `:=` (pop15_17 = round(value*share_pop15_17, 0), pop18_19 = round(value*share_pop18_19, 0))]
-youth <- ry.m.select[,.(agegroups = "15-17", jurisdiction, variable, value = pop15_17),]
-post.youth <- ry.m.select[, .(agegroups = "18-19", jurisdiction, variable, value = pop18_19),]
+setkey(ry.m.select, id)
+setkey(osp.dt1, id)
+ry.m.select[osp.dt1, ':=' (pop15_17 = round(value*share_pop15_17, 0), pop18_19 = round(value*share_pop18_19, 0))]
 
-dts <- list(ry.m, youth, post.youth)
+youth <- ry.m.select[,.(Age = "15-17", County, variable, value = pop15_17),]
+post.youth <- ry.m.select[, .(Age = "18-19", County, variable, value = pop18_19),]
+
+
+# prep final table --------------------------------------------------------
+
+
+dts <- list(ry.m[variable %in% as.character(look.up$gma)], youth, post.youth)
 region.pop.dt <- rbindlist(dts, use.names = TRUE, fill = TRUE)
-format.region.pop.dt <- dcast.data.table(region.pop.dt, agegroups + jurisdiction ~ variable)
+format.region.pop.dt <- dcast(region.pop.dt, Age + County ~ variable)
 
 youth.age <- c('5-9', '10-14', '15-17')
 post.youth.age <- c('18-19', '20-24','25-29', '30-34', '35-39', '40-44','45-49', '50-54','55-59','60-64')
 seniors <- c('65-69', '70-74', '75-79', '80-84')
+age.factor <- c('0-4', youth.age, post.youth.age, seniors, '85+')
+category.factor <- c('0-4', '5-17', '18-64', '65-84', '85+')
 
 # create new column, populate with new cohort labels
-format.region.pop.dt2 <- format.region.pop.dt[agegroups == '0-4', category := "0-4",
-                                               ][agegroups %in% youth.age, category := "5-17",
-                                                 ][agegroups %in% post.youth.age, category := "18-64",
-                                                   ][agegroups %in% seniors, category := "65-84",
-                                                     ][agegroups == '85+', category := "85+",]
+format.region.pop.dt2 <- format.region.pop.dt[, category := fcase(Age == '0-4', "0-4",
+                                                                  Age %in% youth.age, "5-17",
+                                                                  Age %in% post.youth.age, "18-64",
+                                                                  Age %in% seniors, "65-84",
+                                                                  Age == '85+', "85+")]
+## QC ----
+
+format.region.pop.dt2[!is.na(category), 
+                      lapply(.SD, sum), 
+                      .SDcols = str_subset(colnames(format.region.pop.dt2), "^Total"), 
+                      by = County]
 
 # filter where category is not na and only forecast years
-dt0 <- format.region.pop.dt2[!is.na(category), .(category, jurisdiction, tot2010, tot2015, tot2025, tot2040)]
-# dt0 <- format.region.pop.dt2[!is.na(category), .(category, jurisdiction, get(eval(yr.colnames)))]
+dt0 <- format.region.pop.dt2[!is.na(category), 
+                             ][, category := factor(category, levels = category.factor, labels = category.factor)]
 
 # aggregate by county & category
-cnty.dt <- dt0[, lapply(.SD, sum), by = list(category, jurisdiction), .SDcols = c(paste0("tot", forecast.years))]
+cnty.dt <- dt0[, lapply(.SD, sum), by = .(category, County), .SDcols = str_subset(colnames(dt0), "^Total")]
 
 # aggregate by category (region)
-region.dt <- dt0[, lapply(.SD, sum), by = category, .SDcols = c(paste0("tot", forecast.years))][, jurisdiction := 'Central Puget Sound Region']
+region.dt <- dt0[, lapply(.SD, sum), by = category, .SDcols = str_subset(colnames(dt0), "^Total")
+                 ][, County := 'Central Puget Sound Region']
 
 # bind cnty.dt and region.dt
 alldt <- rbindlist(list(cnty.dt, region.dt), use.names = TRUE, fill = TRUE)
 
 # write cnty.dt to workbook
-wb <- createWorkbook(file.path(indir, my.filename))
+wb <- createWorkbook(here('youth-pop-cmp', out.filename))
 modifyBaseFont(wb, fontSize = 10, fontColour = "black", fontName = "Segoe UI")
 addWorksheet(wb, "County Region All Cohorts")
 writeData(wb, sheet = "County Region All Cohorts", x = alldt, colNames = TRUE)
 print("exported County/Region All cohorts")
 
-# # append region.dt to workbook
-# append.worksheet(region.dt, wb, "Region All Cohorts")
-# print("exported Region All cohorts")
-
 # append raw region to workbook
-append.worksheet(region, wb, "OFM 2012 proj for CPS")
+append.worksheet(region, wb, "OFM 2017 proj for CPS")
 print("exported regional OFM projections")
 
 # append censusdt.select to workbook
-append.worksheet(osp.dt1, wb, "OFM 2016 State proj")
+append.worksheet(osp.dt1, wb, "OFM 2020 State proj")
 print("exported OFM State projections for youth")
