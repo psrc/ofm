89b4566304c68b302a9c008138c686b6e22a731e
cyyllam <cyyllam@gmail.com>
2026-02-10 16:55:24 -0800

    revised to catch NAs

--- parcelizer-prep/02-convert-format-saep.R
+++ parcelizer-prep/02-convert-format-saep.R
@@ -1,6 +1,6 @@
 # After running 01-download-saep.R, this script will take original ofm saep format, subset for the Central Puget Sound Region if necessary, 
 # and convert to various formats used at PSRC
-#  revised by Christy Lam 2024-10-17
+#  revised by Christy Lam 2026-02-10
 
 # For QC (comparing April 1 to Block estimates), download from here and save to sub-dir "quality_check/published": https://ofm.wa.gov/washington-data-research/population-demographics/population-estimates/april-1-official-population-estimates
 
@@ -10,14 +10,14 @@ library(openxlsx)
 library(tidyverse)
 
 base.dir <- "J:/OtherData/OFM/SAEP"
-dir <- "SAEP Extract_2024-10-16"
+dir <- "SAEP Extract_2025-11-07"
 data.dir <- file.path(dir, "original")
 filename <- "block20.csv"
 
 id.cols <- c("STATEFP", "COUNTYFP", "TRACTCE", "BLOCKCE", "GEOID20")
 counties <- c("33", "35", "53", "61")
-years <- c(as.character(2010:2019))
-version <- 'September 17, 2024' # taken from OFM block metadata
+years <- c(as.character(2020:2025))
+version <- 'November 7, 2025' # taken from OFM block metadata
 
 # functions ---------------------------------------------------------------
 
@@ -29,6 +29,7 @@ filter.for.psrc <- function(table) {
   cols <- apply(expand.grid(attributes, years), 1, function(x) paste0(x[1], x[2])) # create all combinations of years & attributes
   allcols <- c(id.cols, cols)
   dt <- dt[, ..allcols][, VERSION := version]
+  
 }
 
 convert.file <- function(filename, inputfileformat, outputfileformat){
@@ -37,9 +38,9 @@ convert.file <- function(filename, inputfileformat, outputfileformat){
   if (inputfileformat == "xlsx") df <- read.xlsx(file.path(base.dir, data.dir, filename)) %>% as.data.table()
   if (inputfileformat == "csv")   df <- fread(file.path(base.dir, data.dir, filename)) %>% as.data.table()
   
-  # 2020-24 data format
+  # 2020-25 data format
   dt <- filter.for.psrc(df)
-  
+
   # check for negative values ----
   dt_id_colnames <- c(id.cols, "VERSION")
   dtm <- melt.data.table(dt,
@@ -47,16 +48,24 @@ convert.file <- function(filename, inputfileformat, outputfileformat){
                   measure.vars = setdiff(colnames(dt), dt_id_colnames))
 
   View(dtm[value < 0])
+  dtm[, value := as.numeric(value)]
+  browser()
   dtm[value < 0, value := 0]
 
   check <- dtm[value < 0]
+  nas <- dtm[, any(is.na(value))]
+  if(nas == TRUE) {
+    nas_df <- dtm[is.na(value),]
+    nas_df_m <- dcast.data.table(nas_df, STATEFP + COUNTYFP+ TRACTCE + BLOCKCE + GEOID20 + VERSION ~ variable, value.var = "value")
+    write.xlsx(nas_df_m, "J:\\OtherData\\OFM\\SAEP\\SAEP Extract_2025-11-07\\quality_check\\ofm_saep_qc_2025_11_07_nulls.xlsx")
+  }
   
   # unmelt ----
   dt <- dcast.data.table(dtm, STATEFP + COUNTYFP+ TRACTCE + BLOCKCE + GEOID20 + VERSION ~ variable, value.var = "value")
  
   if (outputfileformat == "dbf") write.dbf(dt, file.path(base.dir, dir, "ofm_saep.dbf"))
-  # if (outputfileformat == "rds") saveRDS(dt, file.path(base.dir, dir, "ofm_saep.rds"))
-  if (outputfileformat == "rds") saveRDS(dt, file.path(base.dir, dir, "ofm_saep_intercensal.rds"))
+  if (outputfileformat == "rds") saveRDS(dt, file.path(base.dir, dir, "ofm_saep.rds"))
+  # if (outputfileformat == "rds") saveRDS(dt, file.path(base.dir, dir, "ofm_saep_intercensal.rds"))
   if (outputfileformat == "csv") write.csv(dt, file.path(base.dir, dir, "ofm_saep.csv"), row.names = FALSE)
 }
 
@@ -89,11 +98,13 @@ read.published.ofm.data <- function() {
 }
 
 qc.rds <- function(years) {
+  
   ofm <- readRDS(file.path(base.dir, dir, "ofm_saep.rds")) %>% as.data.table()
   attributes <- c("POP", "HHP","GQ", "HU", "OHU")
   cols <- apply(expand.grid(attributes, years), 1, function(x) paste0(x[1], x[2]))
   allcols <- c("COUNTYFP", cols)
   odt <- ofm[, allcols, with = F]
+  # browser() 
   dt <- melt.data.table(odt, id.vars = "COUNTYFP", measure.vars = cols, variable.name = "variable", value.name = "estimate")
   dt[, `:=` (attribute = str_extract(variable, "[[:alpha:]]+"), YEAR = str_extract(variable, "[[:digit:]]+"))]
   dtsum <- dt[, lapply(.SD, sum), .SDcols = "estimate", by = .(COUNTY = COUNTYFP, attribute, YEAR)]
@@ -114,15 +125,15 @@ convert.file(filename, inputfileformat = "csv", outputfileformat = "rds")
 
 # read intercensal dataset ----
 
-df <- readRDS(file.path(base.dir, dir, "ofm_saep_intercensal.rds"))
+# df <- readRDS(file.path(base.dir, dir, "ofm_saep_intercensal.rds"))
 
 # QC ----------------------------------------------------------------------
 
 # df <- readRDS(file.path(base.dir, dir, "ofm_saep.rds"))
-
-# years <- c(as.character(2020:2024))
+# 
+# years <- c(as.character(2020:2025))
 # dt <- qc.rds(years)
 # dt
-# write.xlsx(dt, file.path(base.dir, dir, "quality_check", paste0("ofm_saep_qc_", Sys.Date(), "_2.xlsx")))
+# write.xlsx(dt, file.path(base.dir, dir, "quality_check", paste0("ofm_saep_qc_", Sys.Date(), ".xlsx")))
 
 
